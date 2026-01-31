e8bfe20607c6a3960034390a4b0677af75aef15a
aplsamson <142032205+aplsamson@users.noreply.github.com>
2026-01-30 23:41:10 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,4 @@
-// Import Firebase libraries for the Service Worker
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-    apiKey: "AIzaSyCyoUxG8Qfc6g5kwgqRK0Q2J0k1tXtQeVs",
-    authDomain: "cs40-calendar.firebaseapp.com",
-    projectId: "cs40-calendar",
-    storageBucket: "cs40-calendar.firebasestorage.app",
-    messagingSenderId: "661443751516",
-    appId: "1:661443751516:web:ddee708ab5a1014cadc099"
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://www.aviatorgear.com/images/product/large/39950.jpg'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-const CACHE_NAME = 'cs40-calendar-v2';
+const CACHE_NAME = 'cs40-calendar-v1';
 const urlsToCache = [
   './index.html',
   './manifest.json',
